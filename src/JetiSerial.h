2bb6235e1a196d017d84a69298de053adcedf84f
Bedrich Said <bedrich.said@gmail.com>
2022-08-17 08:50:13 +0200

    Added even parity and one stop bit support.

--- src/JetiSerial.h
+++ src/JetiSerial.h
@@ -3,6 +3,15 @@
 uint8_t JETISerial_pin = 19;
 uint16_t JETISerial_delay = 0;
 QueueHandle_t JETISerial_queue;
+uint8_t JETISerial_parity = 0;
+uint8_t JETISerial_stopBits = 0;
+
+#define SERIAL_9N1 0x01
+#define SERIAL_9N2 0x02
+#define SERIAL_9E1 0x11
+#define SERIAL_9E2 0x12
+#define SERIAL_9O1 0x21
+#define SERIAL_9O2 0x22
 
 class JETISerial {
     TaskHandle_t jetiHandle = NULL;
@@ -25,17 +34,24 @@ class JETISerial {
         parity += bitRead(bit9, 0);
         digitalWrite(JETISerial_pin, bit9); // 9th bit
         delayMicroseconds(JETISerial_delay);
-        digitalWrite(JETISerial_pin, parity%2==0); // odd parity
+        if(JETISerial_parity == 1) {
+            digitalWrite(JETISerial_pin, parity%2==1); // even parity
+        }
+        if(JETISerial_parity == 2) {
+            digitalWrite(JETISerial_pin, parity%2==0); // odd parity
+        }
         delayMicroseconds(JETISerial_delay);
-        digitalWrite(JETISerial_pin, HIGH); // 2 stop bits
-        delayMicroseconds(2*JETISerial_delay);
+        digitalWrite(JETISerial_pin, HIGH); // 1 - 2 stop bits
+        delayMicroseconds(JETISerial_stopBits*JETISerial_delay);
     }
 public:
-    void begin(uint16_t speed, uint8_t pin) {
+    void begin(uint16_t speed, uint8_t pin = 19, uint8_t config = SERIAL_9O2) {
         pinMode(pin, OUTPUT);
         digitalWrite(pin, HIGH);
         JETISerial_pin = pin;
         JETISerial_delay = 1000000 / speed; // skarede
+        JETISerial_stopBits = config & 0x01;
+        JETISerial_parity = (config & 0xF0) >> 4;
 
         JETISerial_queue = xQueueCreate( 512, sizeof(uint16_t) );
         xTaskCreatePinnedToCore(this->vTaskCode, "JetiSerial", 10000, NULL, 2, &jetiHandle, 0);
--- src/JetiSerialSingleCore.h
+++ src/JetiSerialSingleCore.h
@@ -1,14 +1,25 @@
 #include <Arduino.h>
 
+#define SERIAL_9N1 0x01
+#define SERIAL_9N2 0x02
+#define SERIAL_9E1 0x11
+#define SERIAL_9E2 0x12
+#define SERIAL_9O1 0x21
+#define SERIAL_9O2 0x22
+
 class JETISerial {
     uint8_t m_pin = 19;
     uint16_t m_delay = 0;
+    uint8_t m_parity = 0;
+    uint8_t m_stopBits = 0;
 public:
-    void begin(uint16_t speed, uint8_t pin) {
+    void begin(uint16_t speed, uint8_t pin = 19, uint8_t config = SERIAL_9O2) {
         pinMode(pin, OUTPUT);
         digitalWrite(pin, HIGH);
         m_pin = pin;
         m_delay = 1000000 / speed; // awful
+        m_stopBits = config & 0x01;
+        m_parity = (config & 0xF0) >> 4;
     }
 
     /**
@@ -29,9 +40,14 @@ public:
         parity += bitRead(bit9, 0);
         digitalWrite(m_pin, bit9); // 9th bit
         delayMicroseconds(m_delay);
-        digitalWrite(m_pin, parity%2==0); // odd parity
+        if(m_parity == 1) {
+            digitalWrite(m_pin, parity%2==1); // even parity
+        }
+        if(m_parity == 2) {
+            digitalWrite(m_pin, parity%2==0); // odd parity
+        }
         delayMicroseconds(m_delay);
-        digitalWrite(m_pin, HIGH); // 2 stop bits
-        delayMicroseconds(2*m_delay);
+        digitalWrite(m_pin, HIGH); // 1 - 2 stop bits
+        delayMicroseconds(m_stopBits*m_delay);
     }
 };
